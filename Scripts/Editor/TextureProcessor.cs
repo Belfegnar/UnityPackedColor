faa2caa4015067f6101aa2a78c552c39d3590b53
Leopotam <leopotam@gmail.com>
2016-12-04 12:06:37 +0300

    [!] Texture processor refactoring, error handling improved [*] YCbCr shader cleanup.

--- Scripts/Editor/TextureProcessor.cs
+++ Scripts/Editor/TextureProcessor.cs
@@ -19,115 +19,129 @@ namespace LeopotamGroup.PackedColor.UnityEditors {
 
         const string SelectLookupFileTitle = "Select lookup filename to save";
 
-		Texture2D[] _sources = new Texture2D[3];
+        readonly static string[] _packingTypeNames =
+            {
+                "YCoCg (Low quality)",
+                "YCbCr (Good quality)",
+            };
 
-		PackedColorType _colorType = PackedColorType.YCoCg;
+        readonly Texture2D[] _sources = new Texture2D[3];
+
+        PackedColorType _colorType = PackedColorType.YCoCg;
 
         void OnEnable () {
             titleContent.text = Title;
         }
 
         void OnGUI () {
+            _colorType = (PackedColorType) EditorGUILayout.Popup ("Packing type", (int) _colorType, _packingTypeNames);
 
-			EditorGUILayout.BeginHorizontal();
-			EditorGUILayout.LabelField ("Textures for R, G and B channels");
-			_colorType = (PackedColorType)EditorGUILayout.EnumPopup (_colorType);
-			EditorGUILayout.EndHorizontal ();
+            _sources[0] = EditorGUILayout.ObjectField ("Texture (R)", _sources[0], typeof (Texture2D), false) as Texture2D;
+            _sources[1] = EditorGUILayout.ObjectField ("Texture (G)", _sources[1], typeof (Texture2D), false) as Texture2D;
+            _sources[2] = EditorGUILayout.ObjectField ("Texture (B)", _sources[2], typeof (Texture2D), false) as Texture2D;
 
-			_sources[0] = EditorGUILayout.ObjectField (_sources[0], typeof (Texture2D), false) as Texture2D;
-			_sources[1] = EditorGUILayout.ObjectField (_sources[1], typeof (Texture2D), false) as Texture2D;
-			_sources[2] = EditorGUILayout.ObjectField (_sources[2], typeof (Texture2D), false) as Texture2D;
-
-            GUI.enabled = _sources[0] != null;
             if (GUILayout.Button ("Process")) {
-				var res = Process (_sources, _colorType);
+                var res = Process (_sources, _colorType);
                 EditorUtility.DisplayDialog (Title, res ?? "Success", "Close");
             }
-            GUI.enabled = true;
         }
 
-		public static string Process (Texture2D[] sources, PackedColorType colorType) {
-			return Process (null, sources, colorType);
+        public static string Process (Texture2D[] sources, PackedColorType colorType) {
+            return Process (null, sources, colorType);
         }
 
-		public static string Process (string path, Texture2D[] sources, PackedColorType colorType) {
+        public static string Process (string path, Texture2D[] sources, PackedColorType colorType) {
+            var width = -1;
+            var height = -1;
+            TextureImporter importer;
+            string gsName = null;
+            foreach (var source in sources) {
+                if (source != null) {
+                    importer = AssetImporter.GetAtPath (AssetDatabase.GetAssetPath (source)) as TextureImporter;
+                    if (importer == null || !importer.isReadable) {
+                        return "Source texture " + source.name + " should be readable";
+                    }
+                    if (width == -1) {
+                        width = source.width;
+                        height = source.height;
+                    }
+                    if (source.width != width || source.height != height) {
+                        return "All textures should be same size";
+                    }
+                    if (gsName != null) {
+                        gsName += "_";
+                    }
+                    gsName += source.name;
+                }
+            }
+
+            if (width == -1) {
+                return "One or more textures should be defined";
+            }
+
             if (path == null) {
                 path = EditorUtility.SaveFolderPanel (SelectFolderTitle, string.Empty, string.Empty);
             }
             if (string.IsNullOrEmpty (path)) {
-                return "Target path no defined";
+                return "Target path not defined";
             }
-            if (sources[0] == null) {
-                return "Source texture not defined";
-            }
-
-            var importer = AssetImporter.GetAtPath (AssetDatabase.GetAssetPath (sources[0])) as TextureImporter;
-            if (importer == null || !importer.isReadable) {
-                return "Source texture should be readable";
-            }
-
-			foreach (Texture2D source in sources) {
-				if (source != sources [0] && source != null) {
-					importer = AssetImporter.GetAtPath (AssetDatabase.GetAssetPath (source)) as TextureImporter;
-					if (importer == null || !importer.isReadable) {
-						return "Source texture " + source.name + " should be readable";
-					}
-					if (source.width != sources [0].width || source.height != sources [0].height) {
-						return "All textures should be same size";
-					}
-				}
-			}
 
             // make folder path relative
             if (path.IndexOf (Application.dataPath) == 0) {
                 path = "Assets" + path.Substring (Application.dataPath.Length);
             }
 
-			Color[] gsData = new Color[sources [0].width * sources [0].height];
-			var tex = new Texture2D (sources[0].width, sources[0].height, TextureFormat.RGB24, false);
-			string fileName = null;
-			for(int s = 0; s < 3; s++)
-			{
-				if(sources[s] == null) continue;
+            var gsData = new Color[width * height];
+            var tex = new Texture2D (width, height, TextureFormat.RGB24, false);
+            string fileName = null;
+            Color[] colData;
+            Color c;
+            float gs;
+            for (var channel = 0; channel < 3; channel++) {
+                if (sources[channel] != null) {
+                    colData = sources[channel].GetPixels ();
+                    var invDataLength = 1 / (float) colData.Length;
+                    for (int i = 0, iMax = colData.Length; i < iMax; i++) {
+                        if (i % 1000 == 0) {
+                            EditorUtility.DisplayProgressBar (Title, string.Format ("Processing {0}...", sources[channel].name), i * invDataLength);
+                        }
+                        c = colData[i];
+                        if (colorType == PackedColorType.YCoCg) {
+                            gs = 0.25f * c.r + 0.5f * c.g + 0.25f * c.b;
+                            colData[i] = new Color (0.5f * c.r - 0.5f * c.b + 0.5f, -0.25f * c.r + 0.5f * c.g - 0.25f * c.b + 0.5f, 0f);
+                            colData[i].b = 1.0f - colData[i].r;
+                        } else {
+                            gs = 0.299f * c.r + 0.587f * c.g + 0.114f * c.b;
+                            colData[i] = new Color (-.168736f * c.r - .331264f * c.g + 0.5f * c.b + 0.5f, 0.5f * c.r - 0.418688f * c.g - 0.081312f * c.b + 0.5f, 0f);
+                            colData[i].b = -0.2989548f * c.r + 0.412898048f * c.g - 0.113943232f * c.b + 0.5f; // Without it dx9 will add 2 alu wtf??
+                        }
+                        gsData[i][channel] = gs;
+                    }
 
-				var colData = sources[s].GetPixels ();
+                    EditorUtility.ClearProgressBar ();
 
-				Color c;
-				float gs;
-				var invDataLength = 1 / (float) colData.Length;
-				for (int i = 0, iMax = colData.Length; i < iMax; i++) {
-					if (i % 1000 == 0) {
-						EditorUtility.DisplayProgressBar (Title, "Processing...", i * invDataLength);
-					}
-					c = colData [i];
-					if (colorType == PackedColorType.YCoCg) {
-						gs = 0.25f * c.r + 0.5f * c.g + 0.25f * c.b;
-						colData [i] = new Color (0.5f * c.r - 0.5f * c.b + 0.5f, -0.25f * c.r + 0.5f * c.g - 0.25f * c.b + 0.5f, 0f);
-						colData [i].b = 1.0f - colData [i].r;
-					} else {
-						gs = 0.299f * c.r + 0.587f * c.g + 0.114f * c.b;
-						colData [i] = new Color (-.168736f * c.r - .331264f * c.g + 0.5f * c.b + 0.5f, 0.5f * c.r - 0.418688f * c.g - 0.081312f * c.b + 0.5f, 0f);
-						colData [i].b = -.2989548f * c.r + 0.412898048f * c.g - 0.113943232f * c.b + .5f;// Without it dx9 will add 2 alu wtf??
-					}
-					gsData [i][s] = gs;
-				}
+                    try {
+                        fileName = AssetDatabase.GenerateUniqueAssetPath (Path.Combine (path, string.Format ("{0}.col.png", sources[channel].name)));
+                        tex.SetPixels (colData);
+                        File.WriteAllBytes (fileName, tex.EncodeToPNG ());
+                    } catch (Exception ex) {
+                        DestroyImmediate (tex);
+                        AssetDatabase.Refresh ();
+                        return ex.Message;
+                    }
+                }
+            }
 
-				EditorUtility.ClearProgressBar ();
-
-				try {
-					fileName = AssetDatabase.GenerateUniqueAssetPath (Path.Combine (path, string.Format ("{0}.col.png", sources[s].name)));
-					tex.SetPixels (colData);
-					File.WriteAllBytes (fileName, tex.EncodeToPNG ());
-				} catch (Exception ex) {
-					DestroyImmediate (tex);
-					return ex.Message;
-				}
-			}
-
-			fileName = AssetDatabase.GenerateUniqueAssetPath (Path.Combine (path, string.Format ("{0}.gs.png", sources[0].name)));
-			tex.SetPixels (gsData);
-			File.WriteAllBytes (fileName, tex.EncodeToPNG ());
-			DestroyImmediate (tex);
+            try {
+                fileName = AssetDatabase.GenerateUniqueAssetPath (Path.Combine (path, string.Format ("{0}.gs.png", gsName)));
+                tex.SetPixels (gsData);
+                File.WriteAllBytes (fileName, tex.EncodeToPNG ());
+                DestroyImmediate (tex);
+            } catch (Exception ex) {
+                DestroyImmediate (tex);
+                AssetDatabase.Refresh ();
+                return ex.Message;
+            }
 
             AssetDatabase.Refresh ();
             return null;
@@ -139,9 +153,8 @@ namespace LeopotamGroup.PackedColor.UnityEditors {
         }
     }
 
-	public enum PackedColorType
-	{
-		YCoCg,
-		YCbCr
-	}
+    public enum PackedColorType {
+        YCoCg = 0,
+        YCbCr
+    }
 }
--- Shaders/PackedColorUnlitOpaque (Good).shader
+++ Shaders/PackedColorUnlitOpaque (Good).shader
@@ -36,8 +36,7 @@ Shader "LeopotamGroup/PackedColor/UnlitOpaque (Good Quality)" {
         }
 
         inline fixed3 YCbCrToRgb (fixed y, fixed3 cbcr) {
-            cbcr = cbcr * fixed3(1.40199995, 1, 1.77199996) - fixed3(0.700999975, 0.5, 0.88599999);
-            return cbcr.rgb + y;
+            return cbcr * fixed3(1.40199995, 1, 1.77199996) - fixed3(0.700999975, 0.5, 0.88599999) + y;
         }
 
         fixed4 frag (v2f i) : SV_Target {
