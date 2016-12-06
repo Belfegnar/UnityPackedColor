1e8f0fb58080240b09d9f166024654e1d937835f
Leopotam <leopotam@gmail.com>
2016-12-06 12:01:27 +0300

    [+] Dithering for luma atlas with multiple methods (Floyd-Steinberg as default). [*] Default packing method switched to YCbCr.

--- Scripts/Editor/TextureProcessor.cs
+++ Scripts/Editor/TextureProcessor.cs
@@ -19,15 +19,59 @@ namespace LeopotamGroup.PackedColor.UnityEditors {
 
         const string SelectLookupFileTitle = "Select lookup filename to save";
 
-        readonly static string[] _packingTypeNames =
+        static readonly string[] _packingTypeNames =
             {
                 "YCoCg (Low quality)",
                 "YCbCr (Good quality)",
             };
 
+        static readonly float[][][] _ditheringCores =
+            {
+                // Floyd-Steinberg
+                new float[][]
+                {
+                    new []{ 0 / 16f, 0 / 16f, 7 / 16f },
+                    new []{ 3 / 16f, 5 / 16f, 1 / 16f },
+                },
+                // Jarvis-Judice-Ninke
+                new float[][]
+                {
+                    new []{ 0 / 48f, 0 / 48f, 0 / 48f, 7 / 48f, 5 / 48f },
+                    new []{ 3 / 48f, 5 / 48f, 7 / 48f, 5 / 48f, 3 / 48f },
+                    new []{ 1 / 48f, 3 / 48f, 5 / 48f, 3 / 48f, 1 / 48f },
+                },
+                // Burkes
+                new float[][]
+                {
+                    new []{ 0 / 32f, 0 / 32f, 0 / 32f, 8 / 32f, 4 / 32f },
+                    new []{ 2 / 32f, 4 / 32f, 8 / 32f, 4 / 32f, 2 / 32f },
+                },
+                // Sierra-3
+                new float[][]
+                {
+                    new []{ 0 / 32f, 0 / 32f, 0 / 32f, 5 / 32f, 3 / 32f },
+                    new []{ 2 / 32f, 4 / 32f, 5 / 32f, 4 / 32f, 2 / 32f },
+                    new []{ 0 / 32f, 2 / 32f, 3 / 32f, 2 / 32f, 0 / 32f },
+                },
+                // Sierra-2
+                new float[][]
+                {
+                    new []{ 0 / 16f, 0 / 16f, 0 / 16f, 4 / 16f, 3 / 16f },
+                    new []{ 1 / 16f, 2 / 16f, 3 / 16f, 2 / 16f, 1 / 16f },
+                },
+                // Sierra-Lite
+                new float[][]
+                {
+                    new []{ 0 / 4f, 0 / 4f, 2 / 4f },
+                    new []{ 1 / 4f, 1 / 4f, 0 / 4f },
+                },
+            };
+
         readonly Texture2D[] _sources = new Texture2D[3];
 
-        PackedColorType _colorType = PackedColorType.YCoCg;
+        PackedColorType _colorType = PackedColorType.YCbCr;
+
+        DitheringType _ditheringType = DitheringType.FloydSteinberg;
 
         void OnEnable () {
             titleContent.text = Title;
@@ -35,22 +79,76 @@ namespace LeopotamGroup.PackedColor.UnityEditors {
 
         void OnGUI () {
             _colorType = (PackedColorType) EditorGUILayout.Popup ("Packing type", (int) _colorType, _packingTypeNames);
-
+            _ditheringType = (DitheringType) EditorGUILayout.EnumPopup ("Dithering", _ditheringType);
             _sources[0] = EditorGUILayout.ObjectField ("Texture (R)", _sources[0], typeof (Texture2D), false) as Texture2D;
             _sources[1] = EditorGUILayout.ObjectField ("Texture (G)", _sources[1], typeof (Texture2D), false) as Texture2D;
             _sources[2] = EditorGUILayout.ObjectField ("Texture (B)", _sources[2], typeof (Texture2D), false) as Texture2D;
 
             if (GUILayout.Button ("Process")) {
-                var res = Process (_sources, _colorType);
+                var res = Process (_sources, _colorType, _ditheringType);
                 EditorUtility.DisplayDialog (Title, res ?? "Success", "Close");
             }
         }
 
-        public static string Process (Texture2D[] sources, PackedColorType colorType) {
-            return Process (null, sources, colorType);
+        static void ProcessDithering (Color[] colData, int width, int height, DitheringType ditheringType) {
+            if (ditheringType == DitheringType.None) {
+                return;
+            }
+
+            const float inv16 = 1 / 16f;
+
+            var core = _ditheringCores[(int) ditheringType - 1];
+            var coreHgt = core.Length;
+            var coreWth = core[0].Length;
+            var coreWthOffset = (coreWth - 1) >> 1;
+
+            float r;
+            float g;
+            float b;
+            int i;
+            int j;
+            int offset;
+
+            for (var y = 0; y < height; y++) {
+                for (var x = 0; x < width; x++) {
+                    offset = y * width + x;
+                    r = colData[offset].r;
+                    g = colData[offset].g;
+                    b = colData[offset].b;
+
+                    // clamping to 4bit pallete colors
+                    colData[offset].r = Mathf.Clamp01 (Mathf.RoundToInt (r * 16) * inv16);
+                    colData[offset].g = Mathf.Clamp01 (Mathf.RoundToInt (g * 16) * inv16);
+                    colData[offset].b = Mathf.Clamp01 (Mathf.RoundToInt (b * 16) * inv16);
+
+                    // finding quantization errors
+                    r -= colData[offset].r;
+                    g -= colData[offset].g;
+                    b -= colData[offset].b;
+
+                    for (var coreY = 0; coreY < coreHgt; coreY++) {
+                        j = y + coreY;
+                        if (j < height) {
+                            for (var coreX = 0; coreX < coreWth; coreX++) {
+                                i = x + coreX - coreWthOffset;
+                                if (i >= 0 && i < width) {
+                                    offset = j * width + i;
+                                    colData[offset].r += core[coreY][coreX] * r;
+                                    colData[offset].g += core[coreY][coreX] * g;
+                                    colData[offset].b += core[coreY][coreX] * b;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-        public static string Process (string path, Texture2D[] sources, PackedColorType colorType) {
+        public static string Process (Texture2D[] sources, PackedColorType colorType, DitheringType ditheringType) {
+            return Process (null, sources, colorType, ditheringType);
+        }
+
+        public static string Process (string path, Texture2D[] sources, PackedColorType colorType, DitheringType ditheringType) {
             var width = -1;
             var height = -1;
             TextureImporter importer;
@@ -66,7 +164,7 @@ namespace LeopotamGroup.PackedColor.UnityEditors {
                         height = source.height;
                     }
                     if (source.width != width || source.height != height) {
-                        return "All textures should be same size";
+                        return "All textures should have same size";
                     }
                     if (gsName != null) {
                         gsName += "_";
@@ -132,6 +230,8 @@ namespace LeopotamGroup.PackedColor.UnityEditors {
                 }
             }
 
+            ProcessDithering (gsData, width, height, ditheringType);
+
             try {
                 fileName = AssetDatabase.GenerateUniqueAssetPath (Path.Combine (path, string.Format ("{0}.gs.png", gsName)));
                 tex.SetPixels (gsData);
@@ -151,6 +251,16 @@ namespace LeopotamGroup.PackedColor.UnityEditors {
         public static void ShowColorProcessDialog () {
             GetWindow<TextureProcessor> ();
         }
+    }
+
+    public enum DitheringType {
+        None = 0,
+        FloydSteinberg,
+        JarvisJudiceNinke,
+        Burkes,
+        Sierra3,
+        Sierra2,
+        SierraLite
     }
 
     public enum PackedColorType {
